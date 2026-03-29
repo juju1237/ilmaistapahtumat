e3ee54dd3f7892bab545eef568c1a5d3b60660b3
juju1237 <juulia.erjanti@gmail.com>
2026-03-29 23:50:18 +0300

    Panic mode

--- items.py
+++ items.py
@@ -2,4 +2,20 @@ import db
 
 def add_item(title, description, date, time, location, user_id):
     sql = "INSERT INTO items (title, description, date, time, location, user_id) VALUES (?, ?, ?, ?, ?, ?)"
-    db.execute(sql, [title, description, date, time, location, user_id])
+    result = db.execute(sql, [title, description, date, time, location, user_id])
+    return result[0] if result else None
+
+def get_items():
+    sql = "SELECT id, title FROM items ORDER BY id DESC"
+    return db.query(sql)
+
+def get_item(item_id):
+    sql = """SELECT items.title,
+    items.description,
+    items.date,
+    items.time,
+    users.username
+    from items, users
+    where items.user_id = users.id AND items.id = ?
+    """
+    return db.query(sql, [item_id])[0]
